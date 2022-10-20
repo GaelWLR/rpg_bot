bfc7d56af8b9fa207237fe22a4022bf9b47b575c
Gaël Weiler <gweiler@kaliop.com>
2022-10-20 11:23:19 +0200

    chore(packages): bump discord.js version to 14

--- src/Client.ts
+++ src/Client.ts
@@ -1,4 +1,4 @@
-import { Client as DiscordClient, Collection, Intents } from 'discord.js';
+import { Client as DiscordClient, Collection, GatewayIntentBits } from 'discord.js';
 
 import { Command } from './types';
 import * as commands from './commands';
@@ -7,7 +7,7 @@ export default class Client extends DiscordClient {
   public commands: Collection<string, Command>;
 
   constructor() {
-    super({ intents: [Intents.FLAGS.GUILDS] });
+    super({ intents: [GatewayIntentBits.Guilds] });
 
     this.commands = new Collection();
 
